4b7dc17341a690e60c39f4fb1d19b5f4ed2cc68e
Sai Sreekar <siddula.s@husky.neu.edu>
2020-05-08 07:17:11 -0400

    heroku trials

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import DashboardContainer from "./components/DashboardContainer";
 
-function App() {
-  return (
-    <DashboardContainer/>
-  );
+class App extends React.Component{
+
+  render() {
+    return (
+        <DashboardContainer/>
+    );
+  }
 }
 
 export default App;
